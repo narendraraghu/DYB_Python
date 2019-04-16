e54b1069f9ae723e45c99dc6f5e92571e0adc3ef
Raghuwanshi, Narendra S <narendra.raghuwanshi@thermofisher.com>
2019-04-16 23:27:54 +0530

    Initial commit

---
+++ HelloWorkd.py
@@ -0,0 +1,11 @@
+def main():
+    print("hello world!")
+print("Guru99")
+
+# It is because we did not declare the call function "if__name__== "__main__".
+
+
+# When Python interpreter reads a source file, it will execute all the code found in it.
+# When Python runs the "source file" as the main program, it sets the special variable (__name__) to have a value ("__main__").
+# When you execute the main function, it will then read the "if" statement and checks whether __name__ does equal to __main__.
+# In Python "if__name__== "__main__" allows you to run the Python files either as reusable modules or standalone programs.
